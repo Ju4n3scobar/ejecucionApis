27bef6521aa7cd95e347c1c17a9111c1cf1a1095
Practicante Desarrollo <prdesarrollo@comodisimos.com.co>
2021-07-22 07:40:52 -0500

    Se llena la base de datos

--- database/seeders/insertBook.php
+++ database/seeders/insertBook.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Books;
 use Illuminate\Database\Seeder;
 
 class insertBook extends Seeder
@@ -13,6 +14,20 @@ class insertBook extends Seeder
      */
     public function run()
     {
+        $book = new Books();
+        $book->title = "1984";
+        $book->description = "Libro de suspenso en el que narra la vida de...";
+        $book->editorial = "UPV";
+        $book->author = "George Orwell";
+        $book->save();
+
+        $book = new Books();
+        $book->title = "Cien años de soledad";
+        $book->description = "Libro melancolico en el que narra la vida de...";
+        $book->editorial = "HMN";
+        $book->author = "Gabriel Garcia Marquez";
+        $book->save();
+        
         
     }
 }
